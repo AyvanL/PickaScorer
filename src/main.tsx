dc9b5139775372ef8fc77e2a7d3796e6cede26df
ayvanl <ayvanzlopez@gmail.com>
2026-05-02 01:11:26 +0800

    fix main

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import SignUp from './pages/SignUp'
-import Login from './pages/Login'
+import Login from './pages/Login.tsx'
 import Homepage from './pages/Homepage'
-import Remote from './pages/Remote'
-import Display from './pages/Display'
+import Remote from './pages/Remote.tsx'
+import Display from './pages/Display.tsx'
 import ProtectedRoute from './ProtectedRoute'
 
 createRoot(document.getElementById('root')!).render(
